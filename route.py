8c33e345c84f9a67964771db902195a68b971ba0
root <masuwonchon@gmail.com>
2017-09-08 18:32:14 +0900

    make flow chart

--- route.py
+++ route.py
@@ -18,16 +18,14 @@ _SUCCESS = 1
 LOG_FILE   = './bigman.log'
 
 app = Flask(__name__)
-app.secret_key = 'random string'
 mysql = MySQL()
-
-app.permanent_session_lifetime = timedelta(minutes=60)
 
 menus = []
 menus.append(['dashboard','fa fa-dashboard fa-fw','Dashboard'])
 menus.append(['view','fa fa-dashboard fa-fw','view'])
 DOMAIN="poloniex.com"
 mysql.init_app(app)
+DATE_FORMAT="%Y/%m/%d/%H/%M/%S/"
 
 # main
 # http://10.50.10.104:5011/dashboard
@@ -40,38 +38,61 @@ def make_chart():
 	str="['Mon',20,28,38,45]"
 	return str
 
+def get_data(command, currencyPair, start, end, period):
+	print_param(command, currencyPair, start, end, period)
+	st = str(int(time.mktime(datetime.datetime.strptime(start,DATE_FORMAT).timetuple())))
+	et = str(int(time.mktime(datetime.datetime.strptime(end,DATE_FORMAT).timetuple())))
+	url = "https://"+DOMAIN+"/public?command="+command+"&currencyPair="+currencyPair+"&start="+st+"&end="+et+"&period="+period
+	resp = requests.get(url)
+	jsonString = json.dumps(resp.text)
+	DATA = json.loads(resp.text)
+
+	if(len(DATA) == 1):
+		print "[ERROR] "+DATA['error']
+		return -1
+
+	return DATA
+
+def print_param(command, currencyPair, start, end, period):
+	print "Date: "+start+" - "+end
+	print "Curr: "+currencyPair
+	print "Cmd : "+command
+	print "Prod: "+period
+	return True
+
 @app.route('/view')
 def view():
-  stime = "00/23/14/07/09/2017"
-  etime = "59/32/14/07/09/2017"
-  command = "returnChartData"
-  currencyPair = "BTC_XMR"
-  period = "300"
-  start = str(int(time.mktime(datetime.datetime.strptime(stime,"%S/%M/%H/%d/%m/%Y").timetuple())))
-  end = str(int(time.mktime(datetime.datetime.strptime(etime,"%S/%M/%H/%d/%m/%Y").timetuple())))
-  url = "https://"+DOMAIN+"/public?command="+command+"&currencyPair="+currencyPair+"&start="+start+"&end="+end+"&period="+period
-  resp = requests.get(url)
-  jsonString = json.dumps(resp.text)
-#  print("jsonString")
-#  print(jsonString)
-  dic = json.loads(resp.text)
-#  dic = {'date':'', 'high':'0', 'low': '0', 'open': '0', 'close': '0'}
+  #  seconds=1, 1min:60, 1hour: 3600, 1day: 14486400
+  #  currencyPair = USDT_XRP, USDT_BTC
+  #  stime = "00/23/14/07/09/2017"
+  #  etime = "59/32/14/07/09/2017"
+  #  period - 5M:300, 15M:900, 30M:1800, 2H:7200, 4H:14400, 1D:86400
+	now = datetime.datetime.now()
+	end = now.strftime(DATE_FORMAT)
+	start_buf = now + datetime.timedelta(minutes=-120)
+	start = start_buf.strftime(DATE_FORMAT)
+	command = "returnChartData"
+	period = "300"
 
-  index=0
-  for i in dic:
-#    print(i['date'])
-#    print(time.asctime(time.localtime(i['date'])))
-#    print(dict[index]['date'])
-#    dict[index]['date'] = time.asctime(time.localtime(i['date']))
-#    print (time.asctime(time.localtime(i['date'])))
-    dic[index]['date'] = "170907142500"
-    index=index+1
+	dic1 = get_data(command, "USDT_XRP", start, end, period)
+	dic2 = get_data(command, "USDT_BTC", start, end, period)
 
-  print "DICT:"+str(dic[0]['date'])
-  print "DICT:"+str(dic[1]['date'])
-  return render_template('view.html', menu=menus, text=dic)
+	index = 0
+	for i in dic1:
+		converted_ticks = now + datetime.timedelta(microseconds = i['date']/10)
+		dic1[index]['date'] = converted_ticks.strftime("%d:%H:%M:%S")
+		index=index+1
+
+	index = 0
+#	print "dic1: "+str(dic1)
+#	print "dic2: "+str(dic2)
+	for j in dic2:
+		converted_ticks = now + datetime.timedelta(microseconds = j['date']/10)
+		dic2[index]['date'] = converted_ticks.strftime("%d:%H:%M:%S")
+		index=index+1
+
+	return render_template('view.html', menu=menus, text1=dic1, text2=dic2)
 
 # main 
 if __name__ == "__main__":
-    app.secret_key = os.urandom(12)
-    app.run(debug=True, host='0.0.0.0', port=5012, threaded=True);
+	app.run(debug=True, host='0.0.0.0', port=5012, threaded=True);
